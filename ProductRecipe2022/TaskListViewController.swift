21ba32ac5890efe01280ca43cbceb281e146dede
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-02-27 12:31:05 +0900

    オートレイアウトバグ削除

---
+++ ProductRecipe2022/TaskListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskListViewController.swift
+//  ProductRecipe2022
+//
+//  Created by SeinaKonishi on 2022/02/21.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
